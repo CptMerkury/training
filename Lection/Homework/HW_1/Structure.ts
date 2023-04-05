736ae635848df9c3862c43f9b15e9bd4461a33fd
maksimakubov <wsdsss@mail.ru>
2023-04-05 20:53:52 +0300

    main: add conspect for first lesson

--- Lection/Homework/HW_1/Structure.ts
+++
@@ -1,8 +0,0 @@
-/* 1.Реализовать структуру на основе массива */
-// const jackBlack = Structure(['name', 'lastName', 'age']);
-
-// jackBlack.set('name', 'Jack');
-// jackBlack.set('lastName', 'Black');
-// jackBlack.set('age', 53);
-
-// jackBlack.get('name') // 'Jack'
