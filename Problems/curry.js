cb444735b27e64bdf649ec834cfab7611c881537
maksimakubov <wsdsss@mail.ru>
2024-02-01 13:07:59 +0300

    main: update curry

--- Problems/curry.js
+++ Problems/curry.js
@@ -16,7 +16,25 @@ function curry(func) {
 
 const carriedSumThree = curry(sumThree);
 
+function sumAll(...a) {
+  const sumA = a.reduce((acc, i) => acc + i, 0);
+  return (...b ) => {
+    if (b.length) {
+      const sumB = b.reduce((acc, i) => acc + i, 0);
+      return sumAll(sumA + sumB)
+    }
+    return sumA;
+  }
+}
+
+
+
 console.log(carriedSumThree(1, 2, 3));
 console.log(carriedSumThree(1, 2)(3));
 console.log(carriedSumThree(1)(2, 3));
 console.log(carriedSumThree(1)(2)(3));
+
+console.log(sumAll(1,2,3)(5)()) //11
+console.log(sumAll(5,5,5)(5)(5)(5)()) //30
+console.log(sumAll(4)(6,3)(2,20)()) //35
+console.log(sumAll(2,3)(100)(4,0)()) //109
