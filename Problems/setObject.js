2fc3de06feb5c2d1c73db5b80c96e28624ce2a7d
maksimakubov <wsdsss@mail.ru>
2024-03-04 17:43:59 +0300

    Add setObj from path

--- Problems/setObject.js
+++ Problems/setObject.js
@@ -15,8 +15,53 @@ function setByPath(target, path, value) {
 
 setByPath(obj, 'foo.bar', 1);
 setByPath(obj, 'foo.bla', 2);
-setByPath(obj, 'foo.baz', 1);
-setByPath(obj, 'foo.bad', 2);
-setByPath(obj, 'foo.val.baz', 4);
+setByPath(obj, 'baz.boo', 1);
+setByPath(obj, 'baz.bad', 2);
+setByPath(obj, 'foo.val', 4);
 
-console.log(obj);
+// console.log(obj);
+
+const prop = {
+  f1: '123',
+  f2: '456',
+}
+
+const dict = {
+  text: 'f6.text',
+  local: 'f6.local.test.p',
+  test: 'f5',
+  address: 'f4'
+}
+
+const data = {
+  f4: 'test',
+  f5: 'test2',
+  f6: {
+    text: 'qwe',
+    local: {
+      test: {
+        p: '123qweqwe'
+      }
+    }
+  }
+}
+
+const getDeep = (data, path) => {
+  if (path.length === 1) {
+    return data[path[0]] || '';
+  }
+  return getDeep(data[path[0]], path.slice(1))
+}
+function getFromPath(target, path, alias, dataSet) {
+  for (const [_, key] of path.entries()){
+    const localPath = alias[key].split('.');
+    if (localPath.length === 1) {
+      target[key] = dataSet[localPath[0]] || ''
+    } else {
+      target[key] = getDeep(dataSet, localPath)
+    }
+  }
+}
+
+getFromPath(prop, ['text', 'test', 'address', 'local'], dict, data)
+console.log(prop)
